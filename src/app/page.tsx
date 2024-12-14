d7560620af61695772857ec86027ac1d4419077c
Stanley Winata <winatastanley355@gmail.com>
2024-12-14 17:50:43 +0700

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 import SplashScreen from "@/app/home/_components/SplashScreen";
 import HomeHeader from "@/app/home/_components/Header";
-import {Button, buttonVariants} from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 
 const Home = async () => {
@@ -10,14 +10,20 @@ const Home = async () => {
     <main>
       {!splashCookie && <SplashScreen />}
       <HomeHeader />
-        <div className="h-screen flex flex-col items-center justify-center px-4 lg:px-0">
-            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-4 text-center">&quot;We bring your ideas
-                into life&quot;</h1>
-            <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight text-center mb-4">
-                INCEPTION.ID
-            </h2>
-            <Link href="/#tellmemore" className={buttonVariants({variant: "default"})}>I have an idea</Link>
-        </div>
+      <div className="h-screen flex flex-col items-center justify-center px-4 lg:px-0">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-4 text-center">
+          &quot;We bring your ideas into life&quot;
+        </h1>
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight text-center mb-4">
+          INCEPTION.ID
+        </h2>
+        <Link
+          href="/#tellmemore"
+          className={buttonVariants({ variant: "default" })}
+        >
+          I have an idea
+        </Link>
+      </div>
     </main>
   );
 };
