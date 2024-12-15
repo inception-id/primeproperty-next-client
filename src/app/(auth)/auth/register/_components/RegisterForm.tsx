e3407ed976a7ef8f957788b5184b4ccc65971831
Stanley Winata <winatastanley355@gmail.com>
2024-12-15 09:13:12 +0700

    Register form UI

---
+++ src/app/(auth)/auth/register/_components/RegisterForm.tsx
@@ -0,0 +1,20 @@
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+
+const RegisterForm = () => {
+    return (
+        <form action="" className="mb-4">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" id="email" name="email" placeholder="Email" required className="mb-4" />
+
+            <Label htmlFor="password">Password</Label>
+            <Input type="password" id="password" name="password" placeholder="Password" required className="mb-4" />
+
+            <Button type="submit" className="w-full">Sign up</Button>
+        </form>
+
+    )
+};
+
+export default RegisterForm;
