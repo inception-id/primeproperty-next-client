925ced786da0422fd3f4ba2f47b20350d6de8b9c
Stanley Winata <winatastanley355@gmail.com>
2024-12-26 13:26:31 +0700

    Prettier

--- src/app/(auth)/auth/login/_components/google-login-button.tsx
+++ src/app/(auth)/auth/login/_components/google-login-button.tsx
@@ -1,67 +1,65 @@
 "use client";
 import { toast } from "react-toastify";
 import {
-    GoogleOAuthProvider,
-    GoogleLogin,
-    CredentialResponse,
+  GoogleOAuthProvider,
+  GoogleLogin,
+  CredentialResponse,
 } from "@react-oauth/google";
-import {env} from "@/lib/env";
-import {decode, JwtPayload} from "jsonwebtoken";
-import {findUser} from "@/lib/api/findUser";
-import {createSupertokensSession} from "@/lib/supertokens/createSupertokensSession";
-import {embedCookieToken} from "@/lib/supertokens/embedCookieToken";
-import {useRouter} from "next/navigation";
+import { env } from "@/lib/env";
+import { decode, JwtPayload } from "jsonwebtoken";
+import { findUser } from "@/lib/api/findUser";
+import { createSupertokensSession } from "@/lib/supertokens/createSupertokensSession";
+import { embedCookieToken } from "@/lib/supertokens/embedCookieToken";
+import { useRouter } from "next/navigation";
 
 const GoogleLoginBtn = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleGoogleLogin = async (credential: CredentialResponse) => {
-        //
-        try {
-            const credentialToken = String(credential.credential);
-            const jwtPayload = decode(credentialToken) as JwtPayload;
-            const user = await findUser(jwtPayload.email);
-            if (!user.data) {
-                toast.error("User does not exist, please register");
-                return;
-            }
+  const handleGoogleLogin = async (credential: CredentialResponse) => {
+    //
+    try {
+      const credentialToken = String(credential.credential);
+      const jwtPayload = decode(credentialToken) as JwtPayload;
+      const user = await findUser(jwtPayload.email);
+      if (!user.data) {
+        toast.error("User does not exist, please register");
+        return;
+      }
 
-            const token = await createSupertokensSession(
-                String(user.data.supertokens_user_id),
-                user.data,
-            );
-            const cookieToken = await embedCookieToken(
-                token.accessToken.token,
-                token.refreshToken.token,
-            );
-            if (cookieToken.accessToken && cookieToken.refreshToken) {
-                toast.success("Sign in successful");
-                router.push("/account");
-                return;
-            }
+      const token = await createSupertokensSession(
+        String(user.data.supertokens_user_id),
+        user.data,
+      );
+      const cookieToken = await embedCookieToken(
+        token.accessToken.token,
+        token.refreshToken.token,
+      );
+      if (cookieToken.accessToken && cookieToken.refreshToken) {
+        toast.success("Sign in successful");
+        router.push("/account");
+        return;
+      }
 
-            toast.error(user.message);
-            return;
-        } catch (error: any) {
-            toast.error("Something went wrong, please try again.");
-            console.error(error.message);
-            return;
-        }
-    };
+      toast.error(user.message);
+      return;
+    } catch (error: any) {
+      toast.error("Something went wrong, please try again.");
+      console.error(error.message);
+      return;
+    }
+  };
 
-    return (
-        <GoogleOAuthProvider
-            clientId={env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
-        >
-            <GoogleLogin
-                locale={navigator.language}
-                onSuccess={handleGoogleLogin}
-                onError={() => toast("Something went wrong, please try again")}
-                text="continue_with"
-                theme="filled_blue"
-            />
-        </GoogleOAuthProvider>
-    );
+  return (
+    <GoogleOAuthProvider clientId={env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
+      <GoogleLogin
+        locale={navigator.language}
+        onSuccess={handleGoogleLogin}
+        onError={() => toast("Something went wrong, please try again")}
+        text="continue_with"
+        theme="filled_blue"
+      />
+    </GoogleOAuthProvider>
+  );
 };
 
 export default GoogleLoginBtn;
--- src/app/(auth)/auth/login/page.tsx
+++ src/app/(auth)/auth/login/page.tsx
@@ -3,24 +3,24 @@ import LoginForm from "@/app/(auth)/auth/login/_components/LoginForm";
 import GoogleLoginBtn from "@/app/(auth)/auth/login/_components/google-login-button";
 
 const LoginPage = () => {
-    return (
-        <main className="flex flex-col items-center justify-center h-screen px-4 lg:px-0">
-            <h1 className="text-center mb-4">INCEPTION</h1>
-            <div className="shadow-lg p-4 rounded-lg w-full max-w-sm">
-                <h2 className="mb-4 font-bold">Sign in</h2>
-                <LoginForm/>
-                <div className="mb-4">
-                    <span>Don&apos;t have an account?</span>
-                    <Link href="/auth/register" className="underline ml-1">
-                        Register
-                    </Link>
-                </div>
-                <div className="flex items-center justify-center">
-                    <GoogleLoginBtn />
-                </div>
-            </div>
-        </main>
-    );
+  return (
+    <main className="flex flex-col items-center justify-center h-screen px-4 lg:px-0">
+      <h1 className="text-center mb-4">INCEPTION</h1>
+      <div className="shadow-lg p-4 rounded-lg w-full max-w-sm">
+        <h2 className="mb-4 font-bold">Sign in</h2>
+        <LoginForm />
+        <div className="mb-4">
+          <span>Don&apos;t have an account?</span>
+          <Link href="/auth/register" className="underline ml-1">
+            Register
+          </Link>
+        </div>
+        <div className="flex items-center justify-center">
+          <GoogleLoginBtn />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default LoginPage;
