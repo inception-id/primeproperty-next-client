e02d95a427ddbcc9b1ad27d4907217cd8d8bc182
Stanley Winata <winatastanley355@gmail.com>
2025-01-04 09:11:30 +0700

    Prettier

--- src/app/(languageai)/languageai/checkbot/_components/checkbot-result.tsx
+++ src/app/(languageai)/languageai/checkbot/_components/checkbot-result.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useContext } from "react";
-import {CheckbotContext} from "@/app/(languageai)/languageai/checkbot/_components/checkbot-provider";
+import { CheckbotContext } from "@/app/(languageai)/languageai/checkbot/_components/checkbot-provider";
 import { UseCompletionHelpers } from "@ai-sdk/react";
 import { LuCopy, LuGripHorizontal } from "react-icons/lu";
 import { Button } from "@/components/ui/button";
@@ -12,26 +12,26 @@ const CheckbotResult = () => {
     useContext<UseCompletionHelpers>(CheckbotContext);
   return (
     <div className="border rounded-md overflow-hidden">
-        <div className="text-xs flex items-center gap-1 p-2 lg:hidden opacity-50">
-            * Drag <LuGripHorizontal/> icon to resize
-        </div>
-        <div className="flex gap-2">
-            <div className="flex-1 text-sm h-[90vh]  lg:h-[95vh] overflow-y-auto p-2">
-                {completion ||
-                    <span className="opacity-50">
+      <div className="text-xs flex items-center gap-1 p-2 lg:hidden opacity-50">
+        * Drag <LuGripHorizontal /> icon to resize
+      </div>
+      <div className="flex gap-2">
+        <div className="flex-1 text-sm h-[90vh]  lg:h-[95vh] overflow-y-auto p-2">
+          {completion || (
+            <span className="opacity-50">
               {isLoading ? "Translating ..." : " Translation will show here"}{" "}
             </span>
-                }
-            </div>
-            <Button
-                type="button"
-                size="icon"
-                variant="ghost"
-                onClick={async () => await copyToClipboard(completion)}
-            >
-                <LuCopy />
-            </Button>
+          )}
         </div>
+        <Button
+          type="button"
+          size="icon"
+          variant="ghost"
+          onClick={async () => await copyToClipboard(completion)}
+        >
+          <LuCopy />
+        </Button>
+      </div>
     </div>
   );
 };
--- src/app/(languageai)/languageai/checkbot/_components/checkbot-textarea.tsx
+++ src/app/(languageai)/languageai/checkbot/_components/checkbot-textarea.tsx
@@ -4,28 +4,28 @@ import { LuX } from "react-icons/lu";
 import { useState } from "react";
 
 const CheckbotTextarea = () => {
-    const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
-    return (
-        <div className="flex h-96 lg:h-[85vh] border rounded-md">
-            <Textarea
-                autoFocus
-                name="checkbot_content"
-                placeholder="Enter text"
-                className="focus-visible:ring-0 focus-visible:ring-offset-0 lg:flex-1 resize-none border-transparent"
-                value={value}
-                onChange={(e)=> setValue(e.target.value)}
-            />
-            <Button
-                size="icon"
-                onClick={() => setValue("")}
-                variant="ghost"
-                type="button"
-            >
-                <LuX />
-            </Button>
-        </div>
-    );
+  return (
+    <div className="flex h-96 lg:h-[85vh] border rounded-md">
+      <Textarea
+        autoFocus
+        name="checkbot_content"
+        placeholder="Enter text"
+        className="focus-visible:ring-0 focus-visible:ring-offset-0 lg:flex-1 resize-none border-transparent"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Button
+        size="icon"
+        onClick={() => setValue("")}
+        variant="ghost"
+        type="button"
+      >
+        <LuX />
+      </Button>
+    </div>
+  );
 };
 
 export default CheckbotTextarea;
