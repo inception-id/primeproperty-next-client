8c6d72fdee13712aaa33e810fbad63833b249800
Stanley Winata <winatastanley355@gmail.com>
2025-02-06 13:57:47 +0700

    Fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { refreshSupertokensSession } from "@/lib/supertokens/refreshSupertokensSession";
 import { decodeJwt } from "jose";
+import {verifySupertokensSession} from "@/lib/supertokens/verifySupertokensSession";
 
 export async function middleware(request: NextRequest) {
   const accessToken = request.cookies.get("accessToken")?.value as string;
@@ -25,12 +26,12 @@ export async function middleware(request: NextRequest) {
   if (new Date().getTime() > Number(jwtPayload.exp) * 1000) {
     const newSession = await refreshSupertokensSession();
     if (newSession?.accessToken?.token && newSession?.refreshToken?.token) {
-      request.cookies.set("accessToken", newSession.accessToken.token);
-      request.cookies.set("refreshToken", newSession.refreshToken.token);
+      response.cookies.set("accessToken", newSession.accessToken.token);
+      response.cookies.set("refreshToken", newSession.refreshToken.token);
       return response;
     } else {
-      request.cookies.delete("accessToken");
-      request.cookies.delete("refreshToken");
+      response.cookies.delete("accessToken");
+      response.cookies.delete("refreshToken");
       return loginRedirect;
     }
   }
