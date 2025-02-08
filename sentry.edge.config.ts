4fee16467fc8ef1f1e1716fc718acd139424bc3f
Stanley Winata <winatastanley355@gmail.com>
2025-02-08 14:48:43 +0700

    Fix sentry dsn

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -7,7 +7,7 @@ import * as Sentry from "@sentry/nextjs";
 import { env } from "@/lib/env";
 
 Sentry.init({
-  dsn: env.SENTRY_DSN,
+  dsn: env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
