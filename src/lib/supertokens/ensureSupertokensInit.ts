9d0ad310a787669a3bc41e40645c80368218b1b8
Stanley Winata <winatastanley355@gmail.com>
2025-02-08 18:19:09 +0700

    Add SMTP env to ci/cd

--- src/lib/supertokens/ensureSupertokensInit.ts
+++ src/lib/supertokens/ensureSupertokensInit.ts
@@ -3,6 +3,7 @@ import Dashboard from "supertokens-node/recipe/dashboard";
 import UserRoles from "supertokens-node/recipe/userroles";
 import SessionNode from "supertokens-node/recipe/session";
 import EmailPasswordNode from 'supertokens-node/recipe/emailpassword'
+import EmailVerificationNode from "supertokens-node/recipe/emailverification";
 import {TypeInput} from "supertokens-node/lib/build/types";
 
 import {env} from "@/lib/env";
@@ -17,7 +18,7 @@ export const supertokensConfig: TypeInput =  {
     appInfo: supertokensAppInfo,
     // recipeList contains all the modules that you want to
     // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
-    recipeList: [SessionNode.init(), Dashboard.init(), UserRoles.init(), EmailPasswordNode.init()],
+    recipeList: [SessionNode.init(), Dashboard.init(), UserRoles.init(), EmailPasswordNode.init(), EmailVerificationNode.init({mode: "OPTIONAL"})],
     isInServerlessEnv: true,
     framework: "custom",
 };
