913d589a44a54f33ba77e343adf9fef9b2a763f1
Stanley Winata <winatastanley355@gmail.com>
2025-02-14 16:02:10 +0700

    Transcription history

--- src/app/(languageai)/languageai/speech-to-text/_components/transcription-result.tsx
+++ src/app/(languageai)/languageai/speech-to-text/_components/transcription-result.tsx
@@ -9,6 +9,7 @@ import { toast } from "react-toastify";
 import { createSpeechToTextStorage } from "@/lib/api/speech-to-text/createTranscriptionStorage";
 import { useLanguageaiSubscriptionStore } from "@/app/(languageai)/_lib/use-languageai-subscription-store";
 import { ELanguageaSubscriptionLimit } from "@/lib/enums/languageai-subscription-limit";
+import LanguageAiSaveToStorageDialog from "@/app/(languageai)/_components/dialogs/save-to-storage";
 
 const TranscriptionResult = () => {
   const { updateSubscriptionStore } = useLanguageaiSubscriptionStore(
@@ -26,11 +27,11 @@ const TranscriptionResult = () => {
       })),
     );
 
-  const onSaveClick = async () => {
+  const onSaveClick = async (title: string) => {
     try {
       const savedTranscript = await createSpeechToTextStorage(
         speechToTextId,
-        text,
+          {title, updated_transcription_text: text},
       );
       if (savedTranscript.status === 402) {
         updateSubscriptionStore(
@@ -78,9 +79,10 @@ const TranscriptionResult = () => {
         >
           <LuCopy />
         </Button>
-        <Button type="button" size="icon" variant="ghost" onClick={onSaveClick}>
-          <LuSave />
-        </Button>
+          <LanguageAiSaveToStorageDialog
+              label="Enter transcription title"
+              onSaveClick={onSaveClick}
+          />
       </div>
     </div>
   );
