77b7f64c2d742b959b37b1185e1e03cf846eb8a6
Stanley Winata <winatastanley355@gmail.com>
2025-05-18 09:09:37 +0700

    Add tooltip to share btn

--- src/app/(client)/properties/[...path]/_components/share-links.tsx
+++ src/app/(client)/properties/[...path]/_components/share-links.tsx
@@ -8,6 +8,7 @@ import { usePathname } from "next/navigation";
 import { env } from "@/lib/env";
 import { MdWhatsapp } from "react-icons/md";
 import { toast } from "react-toastify";
+import { Tooltip } from "react-tooltip";
 
 type ShareLinksProps = {
   title: string;
@@ -28,30 +29,46 @@ export const ShareLinks = ({ title }: ShareLinksProps) => {
         href={facebookUrl}
         target="_blank"
         className={buttonVariants({ size: "icon", variant: "outline" })}
+        data-tooltip-id="fb-share"
+        data-tooltip-content="Share to Facebook"
+        data-tooltip-place="bottom"
       >
         <LuFacebook />
       </Link>
+      <Tooltip id="fb-share" />
       <Link
         href={telegramUrl}
         target="_blank"
         className={buttonVariants({ size: "icon", variant: "outline" })}
+        data-tooltip-id="tele-share"
+        data-tooltip-content="Share to Telegram"
+        data-tooltip-place="bottom"
       >
         <BsTelegram />
       </Link>
+      <Tooltip id="tele-share" />
       <Link
         href={whatsappUrl}
         target="_blank"
         className={buttonVariants({ size: "icon", variant: "outline" })}
+        data-tooltip-id="whatsapp-share"
+        data-tooltip-content="Share to WhatsApp"
+        data-tooltip-place="bottom"
       >
         <MdWhatsapp />
       </Link>
+      <Tooltip id="whatsapp-share" />
       <Link
         href={twitterUrl}
         target="_blank"
         className={buttonVariants({ size: "icon", variant: "outline" })}
+        data-tooltip-id="twitter-share"
+        data-tooltip-content="Share to Twitter"
+        data-tooltip-place="bottom"
       >
         <BsTwitterX />
       </Link>
+      <Tooltip id="twitter-share" />
       <Button
         onClick={() => {
           navigator.clipboard.writeText(url);
@@ -59,9 +76,13 @@ export const ShareLinks = ({ title }: ShareLinksProps) => {
         }}
         size="icon"
         variant="outline"
+        data-tooltip-id="copy-share"
+        data-tooltip-content="Copy Link"
+        data-tooltip-place="bottom"
       >
         <LuCopy />
       </Button>
+      <Tooltip id="copy-share" />
     </div>
   );
 };
