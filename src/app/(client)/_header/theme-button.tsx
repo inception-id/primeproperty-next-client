7b9b805fe022d0a64209352f9ce1ea168f314932
Stanley Winata <winatastanley355@gmail.com>
2025-10-06 20:11:07 +0700

    Update theme button

--- src/app/(client)/_header/theme-button.tsx
+++ src/app/(client)/_header/theme-button.tsx
@@ -1,29 +1,39 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { useTheme } from "next-themes";
-import { MdDarkMode, MdLightMode } from "react-icons/md";
-import { Tooltip } from "react-tooltip";
 
-const ThemeButton = () => {
-  const { resolvedTheme, setTheme } = useTheme();
+import * as React from "react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LuMoon, LuSun } from "react-icons/lu";
+
+export default function ThemeButton() {
+  const { setTheme } = useTheme();
 
   return (
-    <Button
-      aria-label="Toggle Theme"
-      data-tooltip-id="theme-button"
-      data-tooltip-content={resolvedTheme}
-      data-tooltip-place="right"
-      variant="outline"
-      size="icon"
-      onClick={() =>
-        resolvedTheme === "dark" ? setTheme("light") : setTheme("dark")
-      }
-    >
-      {resolvedTheme === "dark" && <MdDarkMode />}
-      {resolvedTheme === "light" && <MdLightMode />}
-      <Tooltip id="theme-button" />
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <LuSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <LuMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
-};
-
-export default ThemeButton;
+}
