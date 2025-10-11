63212dd2f633381db1a8dbc09945046d8719ee85
Stanley Winata <winatastanley355@gmail.com>
2025-10-11 10:18:50 +0700

    Update bio

--- src/app/(client)/agents/[name]/_components/agent-bio.tsx
+++ src/app/(client)/agents/[name]/_components/agent-bio.tsx
@@ -5,7 +5,7 @@ import { BuildingType } from "@/lib/enums/building-type";
 import { env } from "@/lib/env";
 import Image from "next/image";
 import { useMemo } from "react";
-import { LuInstagram, LuMail } from "react-icons/lu";
+import { LuCircleUser, LuInstagram, LuMail } from "react-icons/lu";
 
 export const AgentBio = ({
   agent,
@@ -24,14 +24,18 @@ export const AgentBio = ({
     return {};
   }, [propertiesWithAgent]);
   return (
-    <div className="gap-4 flex flex-col sm:flex-row border p-4 rounded w-fit">
-      <Image
-        alt={agent.fullname}
-        src={`${env.NEXT_PUBLIC_S3_ENDPOINT}${agent.profile_picture_url}`}
-        width={200}
-        height={200}
-        className="rounded-full aspect-square object-cover mx-auto sm:mx-0"
-      />
+    <div className="gap-4 flex flex-col sm:flex-row border p-4 rounded w-fit items-center">
+      {agent.profile_picture_url ? (
+        <Image
+          alt={agent.fullname}
+          src={`${env.NEXT_PUBLIC_S3_ENDPOINT}${agent.profile_picture_url}`}
+          width={200}
+          height={200}
+          className="rounded-full aspect-square object-cover mx-auto sm:mx-0"
+        />
+      ) : (
+        <LuCircleUser className="w-24 h-24 text-primary" />
+      )}
 
       <div className="flex flex-col gap-4">
         <div className="sm:flex flex-col gap-1">
@@ -54,16 +58,22 @@ export const AgentBio = ({
         <div className="flex flex-col gap-2">
           <h2 className="font-bold">Properties Summary</h2>
           <div className="grid grid-cols-2 sm:grid-cols-3 gap-2">
-            {Object.keys(groupedProperties).map((key: any) => (
-              <div key={key} className="flex items-center gap-2">
-                <h3 className="capitalize text-muted-foreground w-28">
-                  {key}:
-                </h3>
-                <div className="font-bold">
-                  {groupedProperties?.[key as BuildingType]?.length}
+            {Object.keys(groupedProperties).length > 0 ? (
+              Object.keys(groupedProperties).map((key: any) => (
+                <div key={key} className="flex items-center gap-2">
+                  <h3 className="capitalize text-muted-foreground w-28">
+                    {key}:
+                  </h3>
+                  <div className="font-bold">
+                    {groupedProperties?.[key as BuildingType]?.length}
+                  </div>
                 </div>
+              ))
+            ) : (
+              <div className="text-muted-foreground text-xs">
+                Belum ada properti diupload
               </div>
-            ))}
+            )}
           </div>
         </div>
       </div>
